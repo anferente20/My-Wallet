b9fd7066383891ea900749960d866b1e1a3c6afc
FelipeR97 <felipe@simian.co>
2021-05-11 21:39:04 -0500

    Agregar gráficas a ingresos y egresos

--- src/app/egresos/egresos.component.ts
+++ src/app/egresos/egresos.component.ts
@@ -1,5 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
 
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+);
 @Component({
   selector: 'app-egresos',
   templateUrl: './egresos.component.html',
@@ -10,6 +60,30 @@ export class EgresosComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+        const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio'];
+        const data = {
+          labels: meses,
+          datasets: [{
+            label: 'Egresos',
+            data: [35, 19, 60, 11, 26, 95, 70],
+            fill: false,
+            borderColor: 'rgb(234, 53, 5)',
+            tension: 0.1
+          }
+          ]
+        };    
+        var myChart = new Chart("egresosChart", {
+            type: 'line',
+            data: data,
+            options: {
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
   }
 
 }
--- src/app/ingresos/ingresos.component.ts
+++ src/app/ingresos/ingresos.component.ts
@@ -1,5 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
 
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+);
 @Component({
   selector: 'app-ingresos',
   templateUrl: './ingresos.component.html',
@@ -10,6 +60,30 @@ export class IngresosComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+      const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio'];
+    const data = {
+      labels: meses,
+      datasets: [{
+        label: 'Ingresos',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(34, 190, 3)',
+        tension: 0.1
+      }
+      ]
+    };    
+    var myChart = new Chart("ingresosChart", {
+        type: 'line',
+        data: data,
+        options: {
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
   }
 
 }
